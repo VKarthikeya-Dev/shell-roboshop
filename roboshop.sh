459ea22c5a67af5a70c34d5af4e5b905554a13ca
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-22 19:24:36 +0530

    Hello instance

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,7 @@ INSTANCES=("mongodb" "user" "catalogue" "frontend" "cart" "redis" "mysql" "shipp
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01ff537c187a3364b --tag-specifications '{"ResourceType":"instance","Tags":[{"Key":"Name","Value":"$instance"}]}' --query 'Instances[*].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01ff537c187a3364b --tag-specifications "ResourceType":"instance","Tags":[{Key=Name,Value=$instance}] --query 'Instances[*].InstanceId' --output text)
     if [ $instance !="frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress'  --output text)
