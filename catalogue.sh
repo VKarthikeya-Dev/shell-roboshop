92eaa9eef8a10ddb4bdee4ab7282f1c10d93b6d7
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-22 21:37:35 +0530

    Catalogue

---
+++ catalogue.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+
+mkdir -p $LOG_FOLDER
+ 
+echo "The script started execution at $(date)" | tee -a $LOG_FILE
+
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R The user doesnt have root access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G The user has root axit can procceed for installation $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 is a success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$R $2 is a failure $N"| tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling Nodejs"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nodejs 20 "
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing Nodejs"
+
+useradd --system --home /app --shell /sbin/nologin --comment"Roboshop user" roboshop &>>$LOG_FILE
+VALIDATE $? "Adding system user roboshop"
+mkdir /app
+VALIDATE $? "Creating a Directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
+VALIDATE $? "Downloading the Zip file in Temp directory"
+cd /app
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping Catalogue"
+npm install &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Setting up system service"
+systemctl daemon-reload
+systemctl start catalogue
+VALIDATE $? "Catalogue start"
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Catalogue enable"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mogosh -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb client"
+
+mongosh --host mongodb.vkdevops.site </app/db/master-data.js &>>$LOG_FILE
