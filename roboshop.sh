c0614a101c9869e0141b4515304116fb1b6c2711
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-23 06:46:45 +0530

    Hello world

--- roboshop.sh
+++ roboshop.sh
@@ -6,16 +6,18 @@ ZONE_ID="Z071068523JSF1PW7XXII"
 DOMAIN_NAME="vkdevops.site"
 INSTANCES=("mongodb" "user" "catalogue" "frontend" "cart" "redis" "mysql" "shipping" "rabbitmq" "payment" "dispatch")
 
-for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01ff537c187a3364b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PrivateIpAddress'  --output text)
         echo "$instance private ip is $IP"
+        RECORD_NAME="$instance.$DOMAIN_NAME"
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress'  --output text)
         echo "$instance public ip is $IP"
+        RECORD_NAME="$DOMAIN_NAME"
     fi
     aws route53 change-resource-record-sets \
   --hosted-zone-id Z071068523JSF1PW7XXII \
@@ -25,7 +27,7 @@ do
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
-            "Name"              : "'$instance'.'$DOMAIN_NAME'"
+            "Name"              : "'$RECORD_NAME'"
             ,"Type"             : "A"
             ,"TTL"              : 1
             ,"ResourceRecords"  : [{
