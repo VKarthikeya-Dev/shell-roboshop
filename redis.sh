0187628c0d83189224088590ca9831c7a85d06cd
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-23 16:16:22 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -44,14 +44,11 @@ VALIDATE $? "Enabling Redis 7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Changing redis conf"
 systemctl start redis 
 systemctl enable redis  &>>$LOG_FILE
 VALIDATE $? "Enabling Redis service"
-
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Changing redis conf"
-systemctl restart redis 
-VALIDATE $? "Restarting Redis service"
 
 
 
