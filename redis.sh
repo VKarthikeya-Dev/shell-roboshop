a4b16d22f451f5d6bdd7992ea07baa9b80bfef02
VKarthikeya-Dev <vkarthikeya1910@gmail.com>
2025-05-23 08:06:44 +0530

    redis

---
+++ redis.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop-log"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+
+
+mkdir -p $LOG_FOLDER
+ 
+echo "The script started execution at $(date)" | tee -a $LOG_FILE
+
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R The user doesnt have root access $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$G The user has root axis can procceed for installation $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$G $2 is a success $N" | tee -a $LOG_FILE
+    else
+        echo -e "$R $2 is a failure $N"| tee -a $LOG_FILE
+    fi
+}
+
+dnf moduele disable redis -y
+VALIDATE $? "Disabling Redis"
+
+dnf module enable redis:7 -y
+VALIDATE $? "Enabling Redis 7"
+
+dnf install redis -y
+VALIDATE $? "Installing Redis"
+
+systemctl start redis -y
+systemctl enable redis -y
+VALIDATE $? "Enabling Redis service"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Changing redis conf"
+systemctl restart redis -y
+VALIDATE $? "Restarting Redis service"
+
+
+
+
